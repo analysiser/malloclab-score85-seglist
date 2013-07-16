67e9a1ade4fc2209d0b11dec59105fcd189db25e
Xiao Li <Analysiser@cmu-853713.wv.cc.cmu.edu>
2013-07-16 16:46:16 -0400

    Initial Commit

---
+++ malloclab/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  malloclab
+//
+//  Created by Xiao Li on 13-7-16.
+//  Copyright (c) 2013年 Xiao Li. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
